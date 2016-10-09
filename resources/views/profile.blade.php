7d676e8aa1edc9e10d36c378191cc390c9100999
MonicaToader1 <m.toader@student.fontys.nl>
2016-10-09 13:52:04 +0200

    Update & view avatar image - not fully functional nor pixelated yet

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,9 +9,22 @@
 
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
+
+
+                    <img src ="/uploads/avatars/ {{$user->avatar}}" style="width: 150px;height: 150px;float: left;-webkit-border-radius: ;-moz-border-radius: ;border-radius: 50%;margin-right: 25px;">
+                    <h2>{{$user->name}}'s Profile</h2>
+                    <form enctype="multipart/form-data" action="/profile" method="POST">
+                        <label>
+                            Update Profile Picture
+                        </label>
+                        <input type="file" name="avatar">
+                        <input type="hidden" anem="_token" value="{{csrf_token()}}">
+                        <input type="Submit" class="pull-right btn btn-sm btn-primary">
+                    </form>
+
+
                     <h1>Edit Profile: {{$user->name}}</h1>
                     <hr>
-
                     {!!Form::model($user, ['route'=> ['profile.update', $user->id], 'method'=>'PATCH'])!!}
                     <div class="form-group">
                         {!! Form::label('nickname', 'Nickname: ') !!}
